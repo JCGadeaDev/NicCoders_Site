d602003e2c11d4595be454877166c5580f61c230
Roberto Calero <rmorck03@gmail.com>
2025-06-23 14:57:49 -0600

    core: update redirections

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [
+          {
+            type: 'host',
+            value: 'niccoders-production.up.railway.app',
+          },
+          {
+            type: 'host',
+            value: 'www.niccoders.com',
+          },
+        ],
+        destination: 'https://niccoders.com',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
